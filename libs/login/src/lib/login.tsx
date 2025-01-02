9984955572979214746c458596b0de120757d64e
mamun2519 <juborajislam46@gmail.com>
2025-01-02 09:40:18 +0600

    explore nx monorepo

--- libs/login/src/lib/login.tsx
+++ libs/login/src/lib/login.tsx
@@ -3,7 +3,7 @@ import styles from './login.module.css';
 export function Login() {
   return (
     <div className={styles['container']}>
-      <h1>Welcome to Login!</h1>
+      <h1 className="text-center text-xl mt-10">Welcome to Login!</h1>
       <p>This Is Login Page</p>
     </div>
   );
