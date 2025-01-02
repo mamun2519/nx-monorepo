48d9560db09f36058cdcaab61b4054bf01b89778
mamun2519 <juborajislam46@gmail.com>
2025-01-02 09:46:21 +0600

    explore nx monorepo

--- libs/login/src/lib/login.tsx
+++ libs/login/src/lib/login.tsx
@@ -1,27 +1,30 @@
+import { Form } from '@remix-run/react';
 import styles from './login.module.css';
 
 export function Login() {
   return (
     <div className={styles['container']}>
       <h1 className="text-center text-xl mt-10">Welcome to Login!</h1>
-      <div className="flex justify-center mt-5 gap-5">
-        <input
-          className="border outline-gray-400 h-10 w-96 p-2"
-          type="text"
-          name="email"
-          placeholder="Enter Your Email"
-          id=""
-        />
+      <Form method="post">
+        <div className="flex justify-center mt-5 gap-5">
+          <input
+            className="border outline-gray-400 h-10 w-96 p-2"
+            type="text"
+            name="email"
+            placeholder="Enter Your Email"
+            id=""
+          />
 
-        <input
-          className="border outline-gray-400 h-10 w-96 p-2"
-          type="text"
-          name="email"
-          placeholder="Enter Your Email"
-          id=""
-        />
-        <button className="bg-blue-600 text-white px-4 rounded">Login</button>
-      </div>
+          <input
+            className="border outline-gray-400 h-10 w-96 p-2"
+            type="text"
+            name="email"
+            placeholder="Enter Your Email"
+            id=""
+          />
+          <button className="bg-blue-600 text-white px-4 rounded">Login</button>
+        </div>
+      </Form>
     </div>
   );
 }
