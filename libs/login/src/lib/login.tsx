aceace8b8b128de4d813fad15b8c8d2ae6b788a7
mamun2519 <juborajislam46@gmail.com>
2025-01-02 08:26:21 +0600

    explore nx monorepo

---
+++ libs/login/src/lib/login.tsx
@@ -0,0 +1,12 @@
+import styles from './login.module.css';
+
+export function Login() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Login!</h1>
+      <p>This Is Login Page</p>
+    </div>
+  );
+}
+
+export default Login;
