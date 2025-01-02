a5ed416d336af82810c272b73f8f5d3db8323bb6
mamun2519 <juborajislam46@gmail.com>
2025-01-02 10:14:05 +0600

    feat: added to login page

--- apps/todo/app/routes/login.tsx
+++ apps/todo/app/routes/login.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Login } from '@nx-monorepo/login';
+
+export const action = async ({ request }: { request: Request }) => {
+  const formData = await request.formData();
+  const email = formData.get('email');
+  const password = formData.get('password');
+
+  return {
+    email,
+    password,
+  };
+};
 
 const LoginPage = () => {
-  return <div>Hello Login Page</div>;
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
 };
 
 export default LoginPage;
