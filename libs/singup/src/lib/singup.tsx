bcf620a37c3f2e0d3af20af7f377a4e4677b2165
mamun2519 <juborajislam46@gmail.com>
2025-01-02 08:41:28 +0600

    explore nx monorepo

---
+++ libs/singup/src/lib/singup.tsx
@@ -0,0 +1,11 @@
+import styles from './singup.module.css';
+
+export function Singup() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Singup!</h1>
+    </div>
+  );
+}
+
+export default Singup;
