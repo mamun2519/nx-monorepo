9f0d64059fde4f61f2636fb51544adcf7ed32104
mamun2519 <juborajislam46@gmail.com>
2025-01-02 09:42:11 +0600

    explore nx monorepo

--- libs/login/src/lib/login.tsx
+++ libs/login/src/lib/login.tsx
@@ -5,7 +5,12 @@ export function Login() {
     <div className={styles['container']}>
       <h1 className="text-center text-xl mt-10">Welcome to Login!</h1>
       <div>
-        <input type="text" name="email" id="" />
+        <input
+          className="border outline-gray-400 h-12 w-96"
+          type="text"
+          name="email"
+          id=""
+        />
       </div>
     </div>
   );
